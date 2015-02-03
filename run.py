daca95dfeb15be4d35ceb0a5b7cefd826f5803ae
Egemen Ay <ayegemen@gmail.com>
2015-02-03 19:19:13 +0100

    changed to development

--- run.py
+++ run.py
@@ -9,6 +9,6 @@ def index():
 
 # This is needed to test localy
 # In the heroku tutorial this code doenst exist plz remove.
-if __name__ == "__main__":
-    app.debug = True
-    app.run()
+# if __name__ == "__main__":
+#     app.debug = True
+#     app.run()
