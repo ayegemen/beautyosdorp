e371e3d7690fef0a7042300a1bd98084e1a61511
Egemen Ay <ayegemen@gmail.com>
2015-02-27 16:17:40 +0100

    spelling check

--- static/js/custom.js
+++ static/js/custom.js
@@ -142,7 +142,7 @@ $(document).ready(function () {
 		thumbnail_navigation    :   0,			// Thumbnail navigation
 		slides 					:	[			// Slideshow Images
 			{image : '/static/images/slider/slide1.jpg',
-				title : '<h2><span>30% korting op alle behandelingen<span></h2>',
+				title : '<h2><span>30% korting op alle behandelingen<span></h2> t/m 31 maart',
 				thumb : '',
 				url : ''
 				},
